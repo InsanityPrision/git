87029fdf78b574d7717b31ef09bc80d2322a5d93
Erik Riquelme <erikone2311@gmail.com>
2024-09-18 17:23:30 +0200

    Add code to main file TypeScript

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+type Character = {
+  name: string;
+  surname: string;
+  age: number;
+};
+
+const pablo: Character = {
+  name: "Pablo",
+  surname: "Lopez",
+  age: 21,
+};
+
+console.log(pablo);
